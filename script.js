2714c3e1e3483fcfec21763a0654fd0d906ebdfc
abhinandansridhar <abhinandansridhar@gmail.com>
2025-06-02 15:03:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,16 @@
-const username = 'abhinandansridhar'; // replace with your GitHub username
-const repo = 'project-veena';
-const apiBase = `https://api.github.com/repos/${username}/${repo}`;
-
-async function getCommitDate(filePath) {
-  const commitsUrl = `${apiBase}/commits?path=${filePath}`;
-  const response = await fetch(commitsUrl);
-  const commits = await response.json();
-  if (commits.length > 0) {
-    return new Date(commits[0].commit.author.date);
-  }
-  return null;
-}
-
-async function calculateStreak() {
-  const contentsUrl = `${apiBase}/contents`;
-  const response = await fetch(contentsUrl);
-  const files = await response.json();
-
-  const dayFiles = files.filter(file => /^day-\d+\.md$/.test(file.name));
-  const dates = [];
-
-  for (const file of dayFiles) {
-    const commitDate = await getCommitDate(file.path);
-    if (commitDate) {
-      dates.push(commitDate);
-    }
-  }
-
-  dates.sort((a, b) => a - b);
-
-  let streak = 0;
-  let currentStreak = 0;
-  let today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  for (let i = dates.length - 1; i >= 0; i--) {
-    const date = dates[i];
-    const diffTime = today - date;
-    const diffDays = diffTime / (1000 * 60 * 60 * 24);
-
-    if (diffDays === 0) {
-      currentStreak = 1;
-    } else if (diffDays === 1 && currentStreak > 0) {
-      currentStreak++;
-    } else {
-      break;
-    }
-
-    today = date;
-  }
-
-  document.getElementById('current-streak').textContent = currentStreak;
-}
-
-calculateStreak();
+fetch('https://api.github.com/repos/YOUR_USERNAME/project-veena/contents')
+  .then(res => res.json())
+  .then(files => {
+    const list = document.getElementById('log-list');
+    files
+      .filter(file => file.name.match(/^day-\d+\.md$/))
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .forEach(file => {
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = file.html_url;
+        link.textContent = file.name.replace('.md', '');
+        li.appendChild(link);
+        list.appendChild(li);
+      });
+  });
