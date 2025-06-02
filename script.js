22f31ee98bb8dd2daf88178d0174e67c7f76e8e5
abhinandansridhar <abhinandansridhar@gmail.com>
2025-06-02 14:49:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('https://api.github.com/repos/abhinandansridhar/project-veena/contents')
+  .then(res => res.json())
+  .then(files => {
+    const list = document.getElementById('log-list');
+    files
+      .filter(file => file.name.match(/^day-\d+\.md$/))
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .forEach(file => {
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = file.html_url;
+        link.textContent = file.name.replace('.md', '');
+        li.appendChild(link);
+        list.appendChild(li);
+      });
+  });
